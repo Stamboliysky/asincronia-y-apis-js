606581918e3e0fcf528bd0c7d243d5c5c7dc5ac8
Stamboliysky <jordanspassoff@gmail.com>
2023-11-09 10:53:31 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,15 @@
-// Escribe tu código aquí:
+// Ejercicio 1: Callbachs
+
+function 
+imprimirHolaMunduConCollback(collback) {
+    setTimeout(function() 
+        
+     {
+        collback('Hola,mundo')
+    }, 2000)
+}
+
+imprimirHolaMunduConCollback(function(mensaje) {
+    console.log(mensaje)
+}) 
+
