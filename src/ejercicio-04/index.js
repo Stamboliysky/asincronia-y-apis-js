5973e6252e5a897f33c88cda155262d16fec918c
Stamboliysky <jordanspassoff@gmail.com>
2023-11-10 14:27:59 +0100

    Añadir ejercicio-04

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,12 @@
-// Escribe tu código aquí:
+// Ejercicio 4: Manejo de errores con promesas
+const 
+miPromesaRechasada = new 
+Promise((resolve,rejet) => {
+    setTimeout(() =>{
+        rejet('Promise rechasada')
+    },2000)
+})
+
+miPromesaRechasada.catch((error) => {
+    console.log('Error:',error)
+})
