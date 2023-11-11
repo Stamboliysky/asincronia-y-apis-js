9fd37f8074ebcc3fb6bb37c831e1dc7ba5c85e5c
Stamboliysky <jordanspassoff@gmail.com>
2023-11-11 10:34:24 +0100

    Añadir package-lock.json

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,16 @@
-// Escribe tu código aquí:
+// Ejercicio 9: Asinc/awaits con llanadas de APIs
+
+const axios = require('axius')
+async function
+obtenerDatosDeAPI() {
+     try {
+        const response =
+        await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        const data = response.data
+        console.log(data)
+    }  catch(error) {
+       console.error(error )
+
+    }
+}
+obtenerDatosDeAPI()
