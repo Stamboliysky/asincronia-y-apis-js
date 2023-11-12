f5cb0e70cf3f249efbaa4d925f61fecc79fece2e
Stamboliysky <jordanspassoff@gmail.com>
2023-11-12 16:27:21 +0100

    Añadir ejercicio-14

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,16 @@
-// Escribe tu código aquí:
+// Ejercicio 14: Encadenamento de Promises con manipulacion de datos
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then((responce) =>
+responce.json())
+.then((data) =>{
+    return data.map ((post) =>post.title.toUpperCase())
+})
+
+.then((titles) => {
+    console.log('Titulos en mayusculas:',titles)
+})
+.catch((error) =>{
+    console.error
+    ('Error:',error)
+})
