c825ccc758c0170dca1087ee9c5907c09b07041a
Stamboliysky <jordanspassoff@gmail.com>
2023-11-12 14:54:13 +0100

    Añadir ejercicio-12

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+// Ejercicio 12: async/await en un bucle
+
+async function procesarElementos(elementos) {
+   for (const elemento of elementos) {
+    await new Promise ((resolve) => setTimeout(resolve,1000) )
+console.log('Procesando elementos',elementos) 
+ }
+}
+
+const elementos = [1,2,3,4,5]
+procesarElementos(elementos)
